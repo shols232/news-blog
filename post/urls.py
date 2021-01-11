140afa0b946e0023ad5d2b9054ec3253f8975a04
shols232 <akinsolaademola232@gmail.com>
2021-01-11 14:42:10 +0100

    fixed deploy error

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import (ListBlogPostsView, CreateBlogPostView, HomePageView, StoreInPostImages, BlogPostDetailView)
+from .views import (ListBlogPostsView, CreateBlogPostView, HomePageView, BlogPostDetailView)
 
 urlpatterns = [
     path('create', CreateBlogPostView.as_view()),
     path('list', ListBlogPostsView.as_view()),
     path('home', HomePageView.as_view()),
     path('detail', BlogPostDetailView.as_view()),
-    path('store-images', StoreInPostImages.as_view())
+    # path('store-images', StoreInPostImages.as_view())
 ]

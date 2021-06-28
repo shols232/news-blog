fbd99bb3753c66ec5672c7ee4f5e41f4cce6d011
shols232 <akinsolaademola232@gmail.com>
2021-06-28 14:21:04 +0100

    FEATURE: added contact us page

--- blog/settings.py
+++ blog/settings.py
@@ -86,20 +86,20 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 WSGI_APPLICATION = 'blog.wsgi.application'
 
 # if DEBUG=="TRUE":
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # else:
-# DATABASES = {
-#     'default':dj_database_url.config(
-#         default=config('DATABASE_URL')
-#     )
-# }
+DATABASES = {
+    'default':dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 # Password validation
